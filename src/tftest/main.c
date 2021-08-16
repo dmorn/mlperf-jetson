17b3ce12676389971cc349a3f23a62e0a93d39a1
jecoz <danielmorandini@me.com>
2021-08-16 15:44:32 +0200

    Load session model from specified folder

--- src/tftest/main.c
+++ src/tftest/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <tensorflow/c/c_api.h>
 
-int main() {
-	char *ver;
+const char *tags = "serve";
+
+int
+usage(char *name) {
+	fprintf(stderr, "usage %s <tensorflow saved model dir path>\n", name);
+	return 1;
+}
+
+int main(int argc, char *argv[]) {
+	char *ver, *dir;
 	int code;
 	TF_Graph* g;
 	TF_DeviceList *devs;
@@ -10,12 +18,17 @@ int main() {
 	TF_Session *sess;
 	TF_SessionOptions *opts;
 
+	if(argc != 2)
+		usage(argv[0]);
+	dir = argv[1];
+
 	ver = TF_Version();
 	printf("Hello from TensorFlow C library version %s\n", ver);
 
 	g = TF_NewGraph();
 	opts = TF_NewSessionOptions();
-	sess = TF_NewSession(g, opts, s);
+
+	sess = TF_LoadSessionFromSavedModel(opts, NULL, dir, &tags, 1, g, NULL, s);
 	if((code = TF_GetCode(s)) != TF_OK) {
 		perror(TF_Message(s));
 		return code;
@@ -37,9 +50,12 @@ int main() {
 		printf("%d: %s\n", i, name);
 	}
 
-	TF_DeleteDeviceList(devs);
-	TF_CloseSession(sess, s);
 	TF_DeleteGraph(g);
+
+	TF_CloseSession(sess, s);
+	TF_DeleteSession(sess, s);
+
+	TF_DeleteDeviceList(devs);
 	TF_DeleteSessionOptions(opts);
 
 	return 0;
