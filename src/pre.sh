9c83b27712dbda99ff1cf6eea0230ed3541a9a9d
jecoz <danielmorandini@me.com>
2021-08-23 15:49:32 +0200

    Change mode on all serial devices

--- src/pre.sh
+++ src/pre.sh
@@ -5,4 +5,9 @@ export TF_CPP_MIN_LOG_LEVEL=2
 sudo systemctl stop serial-getty@ttyS0.service
 sudo systemctl stop serial-getty@ttyGS0.service
 sudo systemctl stop nvgetty.service 
+
 sudo chmod 666 /dev/ttyTHS1
+sudo chmod 666 /dev/ttyS0
+sudo chmod 666 /dev/ttyS1
+sudo chmod 666 /dev/ttyS2
+sudo chmod 666 /dev/ttyGS0
